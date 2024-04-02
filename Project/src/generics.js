8cdac99453f9921388877c4f54315ee911d1dbd3
arpita.sawant <arpita.sawant@coditas.com>
2024-04-03 00:11:30 +0530

    3:52:24

---
+++ Project/src/generics.js
@@ -0,0 +1,17 @@
+var score = [];
+var names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+// identityThree("3");
+// identityThree(3);
+function identityFour(val) {
+    return val;
+}
+identityFour({ brand: "zara", type: 3 });
--- Project/src/generics.ts
+++ Project/src/generics.ts
@@ -13,3 +13,24 @@ function identityTwo(val:any):any{
 function identityThree<Type>(val:Type):Type{
     return val
 }
+
+// identityThree("3");
+// identityThree(3);
+
+function identityFour<T>(val:T):T{
+    return val
+}
+
+interface Bottle{
+    brand:string,
+    type:number
+}
+
+// identityFour<Bottle>({brand:"zara",type:3})
+
+function getSearchProducts<T>(products:T[]):T{
+
+    // some database operations
+    const myIndex =3
+    return products[myIndex]
+}
