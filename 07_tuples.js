c2d347ea5304416305c25ce16747d88e17dc5af9
arpita.sawant <arpita.sawant@coditas.com>
2024-04-02 16:23:12 +0530

    2:12:56

---
+++ 07_tuples.js
@@ -0,0 +1,6 @@
+var tUser;
+tUser = ['a', 1, true];
+var rgb = [255, 255, 255];
+var newUser = [11, "arpita@gmail.com"];
+newUser[1] = "as.com";
+// newUser.push(true);
---
+++ 07_tuples.ts
@@ -0,0 +1,12 @@
+let tUser : [string,number,boolean];
+
+tUser =['a',1,true];
+
+let rgb:[number,number,number] = [255,255,255];
+
+type user = [number,string];
+
+const newUser : user = [11,"arpita@gmail.com"]
+
+newUser[1] = "as.com";
+// newUser.push(true);
