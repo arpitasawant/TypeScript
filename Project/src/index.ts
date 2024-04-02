50eae630ac42e132b9167c51295a196f11a14f5a
arpita.sawant <arpita.sawant@coditas.com>
2024-04-02 21:37:45 +0530

    3:01:17

---
+++ Project/src/index.ts
@@ -0,0 +1,14 @@
+// console.log("typescript is here")
+
+// Classes
+
+class User{
+    email:string
+    name:string
+    constructor(email:string,name:string){
+        this.email = email;
+        this.name = name
+    }
+}
+
+const arpita = new User(email:"a@gmail.com",name:"arpita")
