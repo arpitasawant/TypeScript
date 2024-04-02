6cee7982b02365f3f06b93fdc418d30b5db69349
arpita.sawant <arpita.sawant@coditas.com>
2024-04-03 00:42:22 +0530

    4:14:57

---
+++ Project/src/detection.js
@@ -0,0 +1,14 @@
+// Narrowing
+function detectType(val) {
+    if (typeof val === "string") {
+        return val.toLowerCase();
+    }
+    return val + 3;
+}
+function provideId(id) {
+    if (!id) {
+        console.log("please provide id");
+        return;
+    }
+    id.toLowerCase();
+}
---
+++ Project/src/detection.ts
@@ -0,0 +1,37 @@
+// Narrowing
+
+function detectType(val: number | string) {
+    if (typeof val === "string") {
+        return val.toLowerCase()
+    }
+    return val + 3;
+}
+
+function provideId(id: string | null) {
+    if (!id) {
+        console.log("please provide id")
+        return
+    }
+
+    id.toLowerCase()
+}
+
+
+function printAll(strs: string | string[] | null) {
+
+    if (strs) {
+        if (typeof strs === "object") {
+            for (const s of strs) {
+                console.log(s);
+            }
+
+        } else if (typeof strs === "string") {
+            console.log(strs);
+        }
+    }
+}
+
+interface User{
+    name:string,
+    email:string
+}
