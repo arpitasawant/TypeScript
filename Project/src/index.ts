242514d9583efbc523d9efde991e1c3976e11db1
arpita.sawant <arpita.sawant@coditas.com>
2024-04-02 22:58:09 +0530

    3:22:45

--- Project/src/index.ts
+++ Project/src/index.ts
@@ -1,14 +1,58 @@
-// console.log("typescript is here")
+// // console.log("typescript is here")
 
-// Classes
+// // Classes
 
-class User{
-    email:string
-    name:string
-    constructor(email:string,name:string){
-        this.email = email;
-        this.name = name
+// class User{
+//     email:string
+//     name:string
+//     constructor(email:string,name:string){
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+
+// const arpita = new User(email:"a@gmail.com",name:"arpita");
+// arpita.city = "pune";
+
+// Private and public
+
+
+class User {
+    protected _courseCount = 1;
+    readonly city: string = "goa";
+    constructor(
+        public email: string,
+        private name: string
+    ) {}
+
+    private deleteToken() {
+        console.log("Token deleted");
+    }
+
+    get getAppleEmail(): string {
+        return `apple${this.email}`;
+    }
+
+    
+    get courseCount(): number {
+        return this._courseCount;
+    }
+
+   
+    set courseCount(courseNum: number) {
+        if (courseNum <= 1) {
+            throw new Error("course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 
-const arpita = new User(email:"a@gmail.com",name:"arpita")
+class SubUser extends User {
+    isFamily: boolean = true;
+    
+    changeCourseCount() {
+        this._courseCount = 4; 
+    }
+}
+
+const arpita = new User("a@gmail.com", "arpita");
