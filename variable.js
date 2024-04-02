88b8bc1d6f7cb76103203bb6d56ccd8c9ad6307c
arpita.sawant <arpita.sawant@coditas.com>
2024-04-02 13:31:34 +0530

    Variables

--- variable.js
+++ variable.js
@@ -16,5 +16,11 @@ to cause typechecking errors.
 polyfill -> means the code which is transpile to js
  */
 // Syntax: let variableName:type = value
+// string
 var greetings = "Hello Arpita";
 console.log(greetings);
+// number
+// Numbers are not equivalent to int or float - everything is simply number
+var userId = 37624.333;
+userId.toFixed();
+console.log(userId);
--- variable.ts
+++ variable.ts
@@ -17,7 +17,30 @@ polyfill -> means the code which is transpile to js
 
 // Syntax: let variableName:type = value
 
+// string
+
 let greetings:string = "Hello Arpita";
 console.log(greetings);
 
+// number
+
+// Numbers are not equivalent to int or float - everything is simply number
+
+let userId:number = 37624.333;
+userId.toFixed();
+console.log(userId)
+
+// boolean
+
+let isLoggedIn:boolean = false;
+
+// any : don't use
+
+let hero;
+
+function getHero(){
+    return "hero";
+}
+
+hero = getHero()
 export {}
